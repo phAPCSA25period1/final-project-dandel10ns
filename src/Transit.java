00b1c7458709e1afe6120c866ab9c2c7dba900e5
dandel10ns <jdorlockwood@gmail.com>
2026-05-01 16:41:14 +0000

    Sprint 3 Day 2: Worked on Transit Class adding methods

--- src/Transit.java
+++ src/Transit.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Transit {
     //create a new array
     String[][] lines = {
@@ -6,14 +8,14 @@ public class Transit {
         //Copper Line row 1
         {"Santee", "Gillespie Field", "Arnele Avenue", "El Cajon", " ", " ", " ", " ", " ",},
         //Green Line row 2
-        {"El Cajon", "Grossmont", "SDSU", "Stadium" "Mission Valley Center", "Old Town", "Little Italy", "Gaslamp Quarter", "12th and Imperial"},
+        {"El Cajon", "Grossmont", "SDSU", "Stadium", "Mission Valley Center", "Old Town", "Little Italy", "Gaslamp Quarter", "12th and Imperial"},
         //Orange Line row 3
         {"El Cajon", "Grossmont", "La Mesa Blvd", "Lemon Grove Depot", "Euclid Avenue", "47th Street", "12th and Imperial", "City College", "Courthouse"}
     };
 
     //create a method to choose a destination
     public String whereTo(){
-        Scanner scan = new Scanner(Sysem.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("What trolley stop would you like to go to?");
         int rowNew = lines.findStopRow(scan.nextLine());
@@ -23,7 +25,12 @@ public class Transit {
         int row = lines.findStopRow(scan.nextLine());
         int col = lines.findStopCol(scan.nextLine());
 
-        
+        if(sameLine(row, rowNew)){
+            lines.whichLine(row);
+            System.out.println("Take the ")
+        }
+
+
     }
 
     //loops through lines array and returns index of trolley stop provided
@@ -37,6 +44,7 @@ public class Transit {
         }
     }
 
+    //loops through lines array and returns index of trolley stop provided
     public int findStopCol(String stop){
         for (int row = 0; row < lines.length; row++){
             for (int col = 0; col < lines[0].length; col++){
@@ -46,4 +54,28 @@ public class Transit {
             }
         }
     }
+
+    //find if trolley must switch lines
+    public boolean sameLine(int row, int rowNew) {
+        if (row == rowNew){
+            return true;
+        }
+        return false;
+    }
+
+    public String whichLine(int row){
+        if (row == 0){
+            return ("Blue Line");
+        }
+        else if (row == 1){
+            return ("Copper Line");
+        }
+        else if (row == 1){
+            return ("Green Line");
+        }
+        else if (row == 1){
+            return ("Orange Line");
+        }
+    }
+
 }
